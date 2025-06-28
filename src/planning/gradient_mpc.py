8c7982043aed670a15aeaa97fdb6f3cf4d129de5
Qiandao-Liu <ql342@cornell.edu>
2025-06-28 14:34:16 -0400

    Before warp gradient_based mpc to phystwin_env

--- src/planning/gradient_mpc.py
+++ src/planning/gradient_mpc.py
@@ -1,1 +1,14 @@
 # /workspace/src/planning/gradient_mpc.py
+
+import torch
+import warp as wp
+from tqdm import trange
+import numpy as np
+import pickle
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../PhysTwin")))
+
+from PhysTwin.qqtt.engine.trainer_warp import InvPhyTrainerWarp
+from src.planning.losses import chamfer
+
